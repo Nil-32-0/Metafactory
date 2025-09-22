c39722822c795d620fb95b90a2aacaf37dfb039e
Ethan Lang <ethanc.lang07@gmail.com>
2025-09-22 11:12:11 -0400

    Basic Configuration 1
    Changelog:
    - Updated Mods
    - Added New Mods
    - Began modifying config files
    - Began updating tags of items that should have them

---
+++ minecraft/kubejs/server_scripts/tagging.js
@@ -0,0 +1,6 @@
+ServerEvents.tags("item", event => {
+    event.add("forge:ingots", 
+        "botania:gaia_ingot", "naturesaura:infused_iron", "naturesaura:sky_ingot", "naturesaura:tainted_gold", 
+        "naturesaura:depth_ingot", "tconstruct:cheese_ingot", "avaritia:crystal_matrix_ingot", "avaritia:infinity_ingot",
+        "avaritia:neutron_ingot", "mna:superheated_vinteum_ingot", "mna:superheated_purified_vinteum_ingot")
+})

beb7256c9b19fea11e77912995e51c2272e50425
Ethan Lang <ethanc.lang07@gmail.com>
2026-03-28 13:50:48 -0400

    Add The Aether

--- minecraft/kubejs/startup_scripts/gt/ore_types.js
+++ minecraft/kubejs/startup_scripts/gt/ore_types.js
@@ -4,5 +4,40 @@ GTCEuStartupEvents.registry("gtceu:tag_prefix", event => {
         .baseModelLocation("botania:block/livingrock")
         .unificationEnabled(true)
         .materialIconType(GTMaterialIconType.ore)
-        .generationCondition(ItemGenerationCondition.hasOreProperty)
+        .generationCondition(ItemGenerationCondition.hasOreProperty);
+
+    event.create("holystone", "ore")
+        .stateSupplier(() => Block.getBlock("aether:holystone").defaultBlockState())
+        .baseModelLocation("aether:block/holystone")
+        .unificationEnabled(true)
+        .materialIconType(GTMaterialIconType.ore)
+        .generationCondition(ItemGenerationCondition.hasOreProperty);
+
+    event.create("raw_clorite", "ore")
+        .stateSupplier(() => Block.getBlock("deep_aether:raw_clorite").defaultBlockState())
+        .baseModelLocation("deep_aether:block/raw_clorite")
+        .unificationEnabled(true)
+        .materialIconType(GTMaterialIconType.ore)
+        .generationCondition(ItemGenerationCondition.hasOreProperty);
+
+    event.create("aseterite", "ore")
+        .stateSupplier(() => Block.getBlock("deep_aether:aseterite").defaultBlockState())
+        .baseModelLocation("deep_aether:block/aseterite")
+        .unificationEnabled(true)
+        .materialIconType(GTMaterialIconType.ore)
+        .generationCondition(ItemGenerationCondition.hasOreProperty);
+
+    event.create("divinite", "ore")
+        .stateSupplier(() => Block.getBlock("aether_redux:divinite").defaultBlockState())
+        .baseModelLocation("aether_redux:block/divinite")
+        .unificationEnabled(true)
+        .materialIconType(GTMaterialIconType.ore)
+        .generationCondition(ItemGenerationCondition.hasOreProperty);
+    
+    event.create("sentrite", "ore")
+        .stateSupplier(() => Block.getBlock("aether_redux:sentrite").defaultBlockState())
+        .baseModelLocation("aether_redux:block/sentrite")
+        .unificationEnabled(true)
+        .materialIconType(GTMaterialIconType.ore)
+        .generationCondition(ItemGenerationCondition.hasOreProperty);
 })
--- minecraft/kubejs/startup_scripts/gt/ore_veins.js
+++ minecraft/kubejs/startup_scripts/gt/ore_veins.js
@@ -11,4 +11,14 @@ GTCEuStartupEvents.registry("gtceu:world_gen_layer", event => {
     event.create("alfheim_livingrock_deep")
         .targets("botania:livingrock")
         .dimensions("mythicbotany:alfheim")
+
+    // 60 <= y
+    event.create("aether")
+        .targets("aether:holystone", "deep_aether:raw_clorite", "deep_aether:aseterite", "aether_redux:divinite", "aether_redux:sentrite")
+        .dimensions("aether:the_aether")
+
+    // y < 60
+    event.create("aether_deep")
+        .targets("aether:holystone", "deep_aether:raw_clorite", "deep_aether:aseterite", "aether_redux:divinite", "aether_redux:sentrite")
+        .dimensions("aether:the_aether")
 })
